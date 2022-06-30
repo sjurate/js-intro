9d05ede8db175d67f07bbd7e79267312da0f883f
sjurate <rasaujuratei@gmail.com>
2022-06-30 12:08:03 +0300

    strings

---
+++ js/strings.js
@@ -0,0 +1,29 @@
+/*
+STRINGS
+tekstas - simboliu kratinys/sarasas/masyvas
+teksto iniciavimas:
+- dvygubos kabutes (")
+- viengubos kabutes (')
+- backtick (apostrofas) (`)
+*/
+
+const s12_1 = 'Vienguba kabute(\') ir dvyguba kabute(\").';
+console.log(s12_1);
+const s12_2 = "Vienguba kabute(\') ir dvyguba kabute(\").";
+console.log(s12_2);
+
+const slash1 = 'Slash (\\).';
+console.log(slash1);
+
+const slash2 = "Slash (\\\\).";
+console.log(slash2);
+
+const enter = 'Labas\r\n\
+rytas\r\n\
+Lietuva.';
+console.log(enter);
+
+const backEnter = `Labas
+rytas
+Lietuva.`;
+console.log(backEnter);
