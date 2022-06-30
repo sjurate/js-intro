cbc0abcbc5debef139b2c85338ed636e5eb440bc
sjurate <rasaujuratei@gmail.com>
2022-06-30 11:25:55 +0300

    numbers

---
+++ js/numbers.js
@@ -0,0 +1,115 @@
+
+console.clear();
+/*
+NUMBERS
+normalus:
+    - sveikieji
+    - desimtainiai
+    - teigiami / neigiami
+ne normalus:
+    - Infinity / -Infinity
+    - NaN (not-a-number)
+Kintamuju iniciavimas:
+- const: reiksme negali buti pakeista (default)
+- let: leidziam keisti reiksme
+- var: nenaudoti, jei nenori problemu
+
+Kupranugario sintakse: teigiamaBegalybe;
+
+*/
+
+
+const metai = 2022;
+console.log(metai);
+
+const PI = 3.14;
+const minusSeven = -7;
+
+console.log(PI);
+console.log(minusSeven);
+
+let pinigine = 0;
+console.log(pinigine);
+
+pinigine = 5;
+console.log('Pinigine', pinigine);
+
+
+const a = 5;
+const b = 7;
+
+console.log(a, b, a + b);
+
+
+console.log('suma');
+const suma = a + b;
+console.log(a, b, suma);
+
+
+console.log('skirtumas');
+const skirtumas = a - b;
+console.log(skirtumas);
+
+console.log('sandauga');
+const sandauga = a * b;
+console.log(sandauga);
+
+console.log('dalmuo');
+const dalmuo = a / b;
+console.log(dalmuo);
+
+console.log('liekana');
+const liekana = b % a;
+console.log(liekana);
+
+console.log('laipsnis')
+const laipsnis = a ** b;
+console.log(laipsnis);
+
+console.log(2, 0, 2 ** 0);
+console.log(2, 1, 2 ** 1);
+console.log(2, 2, 2 ** 2);
+console.log(2, 3, 2 ** 3);
+console.log(2, 4, 2 ** 4);
+console.log(2, 5, 2 ** 5);
+console.log(2, 6, 2 ** 6);
+console.log(2, 7, 2 ** 7);
+console.log(2, 8, 2 ** 8);
+console.log(2, 9, 2 ** 9);
+console.log(2, 10, 2 ** 10);
+
+console.log('--------------');
+
+let total = 0;
+
+console.log(total);
+
+total = total + 2;
+
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+console.log(total);
+
+total = total + 2;
+console.log(total);
+total = total + 2;
+console.log(total);
+total = total + 2;
+console.log(total);
+total = total + 2;
+console.log(total);
+
+console.log('Nesamones: NaN');
+console.log(0 / 0);
+console.log(-0 / 0);
+
+console.log(Infinity / Infinity);
+console.log(-Infinity / Infinity);
+
+
+
